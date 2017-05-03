cc7b3df010bc72798de2df88704f7df5e458f065
Nagashree Tadapatri <nagashree.tadapatri@gmail.com>
2017-05-03 12:25:31 +0200

    Initial checkin

---
+++ HelloC++World/src/HelloC++World.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloC++World.cpp
+// Author      : NTadapatri
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello C++ World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
